a2d5d4565dc7d85238f2bf492ae91c55778bc886
Arslan Galimov <arslangalimov44@gmail.com>
2023-10-06 08:12:06 +0500

    demo zip

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 def main():
     print("hello main")
     
-main()
+def demo_zip():
+    values1 = list(range(2, 20))
+    values2 = list(range(10, 29, 3))
+    print(values1)
+    print(values2)
+    
+    for v1, v2 in zip(values1, values2):
+        print("v1 =", v1, "v2 =", v2, "sum =", v1 + v2)
+
+demo_zip()
